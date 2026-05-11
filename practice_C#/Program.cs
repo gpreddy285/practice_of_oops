2c61d6c5c2247da8cab96c2ba9a31667b2d34914
gpreddy <prasad1annareddy@gmail.com>
2026-05-11 11:47:07 +0530

    Add project files.

---
+++ practice_C#/Program.cs
@@ -0,0 +1,28 @@
+﻿using practice_C_;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_C
+    { 
+    class myclass
+    {
+        public void Greet()
+        {
+            Console.WriteLine("Hello, This is method with no parameters and no return type");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            myclass mc = new myclass();
+            mc.Greet();
+            parametersandnoreturntype no=new parametersandnoreturntype();
+            no.add(10, 20);
+            no.greet(10);
+        }
+    }
+}
---
+++ practice_C#/parametersandnoreturntype.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_C_
+{
+    internal class parametersandnoreturntype
+    {
+        public void add(int a,int b)
+        {
+            int c = a + b;
+            Console.WriteLine("The sum of a and b is:" + c);
+        }
+        public void greet(int x)
+        {
+            Console.WriteLine("the value of x is :" + x);
+        }
+    }
+}
